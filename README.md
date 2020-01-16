8430dc990f2830b6d1292e7ff132e7fec8b2fea9
Peaksol <1244050218@qq.com>
2020-01-16 23:18:42 +0800

    New feature: custom patterns
    Please report any bugs that you found

--- README.md
+++ README.md
@@ -1,13 +1,17 @@
-# NicknameGenerator
-A simple tool to generate random nicknames. [Click Here](https://travindreek.github.io/NicknameGenerator/index.html)
 
-What generated names are like:
+# NicknameGenerator
+A simple tool to generate random nicknames. [Click Here](https://travindreek.github.io/NicknameGenerator)
+
+## How to use it
+Click "Generate" to generate a random username.
+If you would like to use custom patterns, simply click "Settings" and set your own. You can use "{adj}", "{n}" or "{num}" that will be replaced with random adjectives, nouns or numbers.
+
+## What generated names are like
 ```
-TheIdealCity
-BetterGamerTrade
-ShaggyTrain95
-DrinkAndBehalf
-KeenGamerEye
-MC_Jumpy86
+GreyChest75
+IslandAndAction
+TheBadPark
+KaputTour90
+TheUppityWriter
 ...
 ```
--- script.js
+++ script.js
@@ -1,11 +1,47 @@
 console.info('%c➤ Nickname Generator by Peaksol', 'color: #2196F3; font-size: 20px')
+
+function Settings(){
+	// "Settings" button
+	let input = document.getElementById('input');
+	if(localStorage.getItem('custom')){
+		input.value = localStorage.getItem('custom');
+	}
+	else {
+		Reset();
+		localStorage.setItem('custom', input.value);
+	}
+	document.getElementById('main').classList.add("hidden");
+	document.getElementById('settings').classList.remove("hidden");
+}
+
 function Generate(){
-	document.getElementById('output').value = GetNick();
+	// "Generate" button
+	let result = GetNick(localStorage.getItem('custom').split("\n"));
+	if(result){
+		document.getElementById('output').value = result;
+	}
+	else {
+		alert('Couldn\'t generate a vaild name. Make sure your custom patterns are vaild and not too long.');
+	}
 }
 
 function Copy(){
+	// Copy the result when it's clicked
 	output.select();
 	if(output.value!='' && document.execCommand('copy')){
 		alert('Name Copied!');
 	}
+}
+
+function Reset(){
+	// "Reset" button
+	document.getElementById('input').value = "{adj}{n}{num}\n{n}And{n}\nThe{adj}{n}\n{adj}{n}Gamer\n{n}For{n}\n{adj}{n}HD\n{n}Is{adj}"
+}
+
+function Apply(){
+	// "Apply" button
+	let input = document.getElementById('input');
+	localStorage.setItem('custom', input.value.replace(/\s+$/g, ''));
+	document.getElementById('main').classList.remove('hidden');
+	document.getElementById('settings').classList.add('hidden');
 }
